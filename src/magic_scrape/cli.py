a48e0e8e64f9a4ee198a567d65295f46db880858
Louis Maddox <louismmx@gmail.com>
2023-08-06 13:26:16 +0100

    feat: add CLI using Pydantic to decompose CLI into constitutent parts

--- src/magic_scrape/cli.py
+++ src/magic_scrape/cli.py
@@ -1,30 +1,45 @@
 from __future__ import annotations
 
 import defopt
-from pydantic import BaseModel, Field, ValidationError, model_validator
+from pydantic import BaseModel, Field, ValidationError, field_validator
 from pydantic_settings import BaseSettings
 
-__all__ = ["Settings", "APIConfig"]
+__all__ = ["OpenAI", "APIConfig", "main"]
 
 
-class Settings(BaseSettings):
-    openai_api_key: str = Field(..., env="OPENAI_API_KEY")
+class OpenAI(BaseSettings):
+    key: str = Field(..., validation_alias="openai_api_key")
 
 
 class APIConfig(BaseModel):
-    openai_api_key: str | None = Field(None, env="OPENAI_API_KEY")
+    openai_api_key: str | None = None
 
-    @model_validator(mode="before")
-    def check_openai_api_key(cls, values):
-        openai_api_key = values.get("openai_api_key")
-        if not openai_api_key:
-            try:
-                settings = Settings()
-                openai_api_key = settings.openai_api_key
-                values["openai_api_key"] = openai_api_key
-            except ValidationError as ve:
-                raise ValueError("openai_api_key is missing!") from ve
-        return values
+    @field_validator("openai_api_key", mode="before")
+    @classmethod
+    def check_openai_api_key(cls, v: str | None):
+        try:
+            return v or OpenAI().key
+        except ValidationError:
+            raise ValueError("OpenAI API key is missing!")
 
 
-defopt.run(APIConfig)
+class ScraperConfig(BaseModel):
+    url: str
+
+
+class CLIConfig(ScraperConfig, APIConfig):
+    """
+    Configure both the API key auth and web scrape settings.
+    """
+
+
+def main():
+    config = defopt.run(CLIConfig)
+    print("Loaded CLI config:", config)
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except ValidationError as ve:
+        print(ve)
