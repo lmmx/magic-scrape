9576632d8eb83b0f450f9bb9a4ec1c5d1dfeb44f
Louis Maddox <lmmx@users.noreply.github.com>
2023-08-08 01:12:18 +0100

    feat: initial web scraping abilities (#7)
    * style: rename cli_context to cli_config_context in preparation to add other CLI features
    
    * refactor(cli): move configuration-related classes and context to config.py
    
    feat(config): add new configuration management module
    
    chore: update __all__ variable in cli.py
    
    * feat(cli): integrate detect_selectors into main function
    
    feat(config): add targets attribute to ScraperConfig
    
    feat(mock): introduce mock magic_page content
    
    feat(scrape): add selector detection logic with mock data integration
    
    chore: prepare for web scraping and automated pattern extraction
    
    * feat(cli): add verbose mode and refactor logging
    
    refactor(log): err function replaces STDERR print statements
    
    test(cli_test): update tests to accommodate new CLI targets argument
    
    chore: improve CLI and logging for test compatibility
    
    * test: mock HTTP responses and update tests to use mock data
    
    feat: use httpx for real HTTP requests in scrape.py
    
    chore: add httpx to package dependencies
    
    chore: update .pre-commit-config.yaml to exclude tests/data
    
    refactor: remove mock.py as it's no longer needed
    
    * feat: enhance web scraping capabilities
    
    chore: add dependencies for XML parsing: pydantic-xml & lxml.
    
    test: include pytest-httpx for mocking HTTP responses in tests.
    
    feat: introduce BaseXmlModel derived classes for parsing XML sitemaps: UrlSet & Url.
    
    refactor: get_first_page to parse sitemaps and extract the first URL.
    
    fix: improve error handling in get_first_page to identify unsupported namespaces.
    
    test: extend test fixtures with dummy sitemap and related URL data.
    
    test: cover the full scraping process, including sitemap fetching and URL extraction.
    
    * test: fix failing tests after introducing new mocks and fixtures
    
    * fix: all tests pass again (ai_extract and detect_selectors were confused)

--- tests/cli_test.py
+++ tests/cli_test.py
@@ -1,51 +1,60 @@
-from unittest import mock
+from unittest.mock import patch
 
 import pytest
 from pytest import mark
 
 from magic_scrape.cli import CLIConfig, main
 
-# from pydantic import ValidationError
+from .helpers.extraction import mock_extract
 
 
-def run_cli(*args, **env_vars):
+def run_cli(*args, **kwargs):
     """Helper function to run the CLI command."""
-    with mock.patch("sys.argv", ["prog_name"] + list(args)):
-        with mock.patch.dict("os.environ", env_vars):
-            return main(debug=True)
+    patched_page = kwargs.pop("patched_page")
+    env_vars = kwargs
+    with patch("sys.argv", ["prog_name"] + list(args)):
+        with patch.dict("os.environ", env_vars):
+            with patch("magic_scrape.scrape.get_first_page", return_value=patched_page):
+                with patch("magic_scrape.scrape.ai_extract", new=mock_extract):
+                    return main(debug=True)
 
 
 @mark.parametrize("key,url", [("a_key", "http://example.com")])
-def test_openai_api_key_cli_arg(key, url):
+def test_openai_api_key_cli_arg(key, url, fake_page):
     """Test when API key is provided via command line argument."""
-    args = ["--openai-api-key", key, "--url", url]
-    result = run_cli(*args)
-    expected = CLIConfig(openai_api_key=key, url=url)
+    targets = "animal", "clothing"
+    args = ["--openai-api-key", key, "--url", url, "--targets", *targets]
+    result = run_cli(*args, patched_page=fake_page)
+    # Note: this is used as the `fake_config` fixture in helpers.fixtures
+    expected = CLIConfig(openai_api_key=key, url=url, targets=targets)
     assert result.config == expected
 
 
 @mark.parametrize("key,url", [("e_key", "http://example.com")])
-def test_openai_api_key_env_var(key, url):
+def test_openai_api_key_env_var(key, url, fake_page):
     """Test when API key is provided via environment variable."""
-    args = ["--url", url]
+    targets = "animal", "clothing"
+    args = ["--url", url, "--targets", *targets]
     env_vars = {"OPENAI_API_KEY": key}
-    result = run_cli(*args, **env_vars)
-    expected = CLIConfig(openai_api_key=key, url=url)
+    result = run_cli(*args, patched_page=fake_page, **env_vars)
+    expected = CLIConfig(openai_api_key=key, url=url, targets=targets)
     assert result.config == expected
 
 
 @mark.parametrize("arg_key,env_key,url", [("a_key", "e_key", "http://example.com")])
-def test_openai_api_key_cli_arg_and_env_var(arg_key, env_key, url):
+def test_openai_api_key_cli_arg_and_env_var(arg_key, env_key, url, fake_page):
     """Test CLI argument overrides environment variable when both set the API key."""
-    args = ["--openai-api-key", arg_key, "--url", url]
+    targets = "animal", "clothing"
+    args = ["--openai-api-key", arg_key, "--url", url, "--targets", *targets]
     env_vars = {"OPENAI_API_KEY": env_key}
-    result = run_cli(*args, **env_vars)
-    expected = CLIConfig(openai_api_key=arg_key, url=url)
+    result = run_cli(*args, patched_page=fake_page, **env_vars)
+    expected = CLIConfig(openai_api_key=arg_key, url=url, targets=targets)
     assert result.config == expected
 
 
-def test_missing_openai_api_key():
+def test_missing_openai_api_key(fake_page):
     """Test when API key is missing in both command line and environment variable."""
-    args = ["--url", "http://example.com"]
+    targets = "animal", "clothing"
+    args = ["--url", "http://example.com", "--targets", *targets]
     with pytest.raises(ValueError, match="OpenAI API key is missing"):
-        run_cli(*args)
+        run_cli(*args, patched_page=fake_page)
